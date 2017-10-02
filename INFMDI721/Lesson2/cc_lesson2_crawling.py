fe6cb06d2f13d779452a58ac99e88b3cbe9d6e61
belinguc <belinguier.christopher@gmail.com>
2017-10-02 17:48:28 +0200

    cc_Lesson2

--- INFMDI721/Lesson2/cc_lesson2_crawling.py
+++ INFMDI721/Lesson2/cc_lesson2_crawling.py
@@ -12,31 +12,28 @@ def get_soup_from_url(my_url, my_class):
     return soup
 
 
-def compute_discount(url):
+def compute_discount(brand):
     """Attention il faut voir si prix reduit ou pas si oui calculer la diff sinon 0 + recuperer id produit.
     Voir comment se naviguer dans un dom avec les bonnes methodes. (.param pour remonter dans les parents)"""
-    list_discount = []
-    soup = get_soup_from_url(url, 'prdtBZPrice')
-    if soup:
-        price = soup.find_all(class_='prdtPrice')
-        for val in price:
-            price = float(val.text.replace('\xa0', '').replace('\u20ac', '.').replace(' ', '').replace(',', '.'))
-            if val.parent.select_one("[class~=prdtPrSt]"):
-                old_price = float(val.parent.select("[class~=prdtPrSt]")[0].text.replace('\xa0', '').replace('\u20ac', '.').replace(' ', '').replace(',', '.'))
-                discount = (old_price - price)/old_price
-                list_discount.append(discount)
-            else:
-                list_discount.append(0)
-        average_discount = np.average(list_discount) * 100
-        return round(average_discount, 2)
-    else:
-        return 0
+    for num_page in range(1,30):
+        url = "https://www.cdiscount.com/informatique/ordinateurs-pc-portables/pc-portables/lf-228394_6-" + brand + ".html#_his_";
+        list_discount = []
+        soup = get_soup_from_url(url, 'prdtBZPrice')
+        if soup:
+            price = soup.find_all(class_='prdtPrice')
+            for val in price:
+                price = float(val.text.replace('\xa0', '').replace('\u20ac', '.').replace(' ', '').replace(',', '.'))
+                if val.parent.select_one("[class~=prdtPrSt]"):
+                    old_price = float(val.parent.select("[class~=prdtPrSt]")[0].text.replace('\xa0', '').replace('\u20ac', '.').replace(' ', '').replace(',', '.'))
+                    discount = (old_price - price)/old_price
+                    list_discount.append(discount)
+                else:
+                    list_discount.append(0)
+            average_discount = np.average(list_discount) * 100
+            return round(average_discount, 2)
+        else:
+            return 0
 
 
-
-url_acer = "https://www.cdiscount.com/informatique/ordinateurs-pc-portables/pc-portables/lf-228394_6-acer.html#_his_"
-url_dell = "https://www.cdiscount.com/informatique/ordinateurs-pc-portables/pc-portables/lf-228394_6-dell.html#_his_"
-
-
-print("Average discount on Acer computer : " + str(compute_discount(url_acer)) + "%")
-print("Average discount on Dell computer : " + str(compute_discount(url_dell)) + "%")
+print("Average discount on Acer computer : " + str(compute_discount('acer')) + "%")
+print("Average discount on Dell computer : " + str(compute_discount('dell')) + "%")

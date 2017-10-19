d4b2ce74e0996abcca741e18ae91c70283a1c5cf
belinguc <belinguier.christopher@gmail.com>
2017-10-19 23:15:31 +0200

    Lesson4

--- INFMDI721/Lesson4/exo_dom_lesson4.py
+++ INFMDI721/Lesson4/exo_dom_lesson4.py
@@ -1,7 +1,7 @@
 import requests
 import re
-import json
 import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup
 
 
@@ -15,6 +15,7 @@ voiture est plus chère ou moins chère que sa cote moyenne.﻿"""
 
 REGIONS = ['aquitaine', 'ile_de_france', 'provence_alpes_cote_d_azur']
 URL_CENTRALE = 'http://www.lacentrale.fr/cote-voitures-renault-zoe--2013-.html'
+VERSION_ZOE = ['intens', 'life', 'zen']
 
 
 def get_soup_from_url(url):
@@ -54,25 +55,51 @@ def get_details(link_detail):
     description = re.sub(r"\W", "", details[-1].text.lower())
     # print(description)
     version = "NaN"
-    if "intens" in description:
-        version = "intens"
-    elif "life" in description:
-        version = "life"
-    elif "zen" in description:
-        version = "zen"
+    if VERSION_ZOE[0] in description:
+        version = VERSION_ZOE[0]
+    elif VERSION_ZOE[1] in description:
+        version = VERSION_ZOE[1]
+    elif VERSION_ZOE[2] in description:
+        version = VERSION_ZOE[2]
     print(version)
+    comparateur = "NaN"
+    if version != "NaN":
+        argus_key = str(annee) + '-' + str(version)
+        argus = dict_argus[argus_key]
+        if prix < argus:
+            comparateur = True
+        else:
+            comparateur = False
+    else:
+        argus = "NaN"
     # telephone = soup.find_all(class_='phone_number')
     # print(telephone)
-    return [annee, version, prix, kilometrage, vendeur]
+    return [annee, version, prix, kilometrage, vendeur, argus, comparateur]
+
+
+def get_argus(year_min, year_max):
+    dict_argus = {}
+    for year in np.arange(year_min, year_max):
+        for version in VERSION_ZOE:
+            url = "https://www.lacentrale.fr/cote-auto-renault-zoe-" + version + "+charge+rapide-" + str(year) + ".html"
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            argus = int(soup.find_all('span', class_='jsRefinedQuot')[0].text.strip().replace(" ", ""))
+            key = str(year) + '-' + str(version)
+            dict_argus[key] = argus
+    return dict_argus
+
 
 
 if __name__ == '__main__':
     # Crawling
     for region in REGIONS:
+        # Centrale
+        dict_argus = get_argus(2012, 2018)
+
         # Le bon coin
         print("Region : " + str(region))
         nb_pages = get_nb_pages('https://www.leboncoin.fr/voitures/offres/' + str(region) + '/?o=1&brd=Renault&mdl=Zoe')
-        # print(nb_pages)
         fichier = []
         for i in range(1, nb_pages+1):
             url_region = 'https://www.leboncoin.fr/voitures/offres/' + str(region) + '/?o=' + str(i) + '&brd=Renault&mdl=Zoe'
@@ -80,18 +107,10 @@ if __name__ == '__main__':
             for link in link_details:
                 fichier.append(get_details(link))
         print("length fichier : " + str(len(fichier)))
-        labels = ['Annee', 'Version', 'Prix', 'Kilometrage', 'Vendeur']
+        labels = ['Annee', 'Version', 'Prix', 'Kilometrage', 'Vendeur', 'argus', 'comparateur']
         df = pd.DataFrame.from_records(fichier, columns=labels)
-        df = df.sort_values(['Annee', 'Version', 'Prix', 'Kilometrage', 'Vendeur'])
+        df = df.sort_values(['Annee', 'Version', 'Prix', 'Kilometrage', 'Vendeur', 'argus', 'comparateur'])
         print(df)
-        # Centrale
-        # Ajouter colonne centrale +  groupe modele ou année...
-        # URL_CENTRALE
 
         # Sauvegarde
         df.to_csv(region + '.csv')
-
-# Travail restant, ajouter :
-#     numero tel
-#     prix de l'Argus du modèle
-#     bool voiture est plus chère ou moins chère que sa cote moyenne
